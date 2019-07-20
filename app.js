f15d959167afdd509bc4a472a90134395ef92b9c
Brian.L <brian9500885@gmail.com>
2019-07-21 02:41:31 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000)
